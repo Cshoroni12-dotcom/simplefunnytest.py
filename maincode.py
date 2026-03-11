de8a0289edbb602385395ee4599657fbd5bb4b2b
Scory237 <Cshoroni12@gmail.com>
2026-03-11 18:03:57 +0900

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,26 +1,47 @@
 score = 0
+hp = 3
 
 def check(correct, answer):
     global score
+    global hp
     if correct == answer:
         print("you got it right!")
         score += 1
+        return True
     else:
-        print("nope")
+        hp -= 1
+        print("nope, you lose a life, try again")
+        print(f"you have {hp} lives remaining")
+        return False
 
-answer = input("cycle cycle cycle > ")
-check("tricycle", answer)
 
-answer = input("stand stand stand i > ")
-check("understand", answer)
+while hp > 0:
+    answer = input("cycle cycle cycle > ")
+    if check("tricycle", answer):
+        break
+hp = 3
 
-answer = input("man board > ")
-check("manoverboard", answer)
+while hp > 0:
+    answer = input("stand stand stand i > ")
+    if check("understand", answer):
+        break
+hp = 3
 
-answer = input("you just me > ")
-check("just_between_you_and_me", answer)
+while hp > 0:
+    answer = input("man board > ")
+    if check("manoverboard", answer):
+        break
+hp = 3
 
-answer = input("space space space > ")
-check("outerspace", answer)
+while hp > 0:
+    answer = input("you just me > ")
+    if check("just_between_you_and_me", answer):
+        break
+hp = 3
+
+while hp > 0:
+    answer = input("space space space > ")
+    if check("outerspace", answer):
+        break
 
 print("Final score:", score)
